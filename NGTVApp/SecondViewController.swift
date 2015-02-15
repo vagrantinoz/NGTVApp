66412ec4a2624b7ff6f22fc28a8b89cabfc65d01
Lunafei <lunafei@gmail.com>
2015-02-15 19:43:33 +0900

    Initial Commit

---
+++ NGTVApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NGTVApp
+//
+//  Created by lunafei on 2015. 2. 15..
+//  Copyright (c) 2015년 lunafei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
